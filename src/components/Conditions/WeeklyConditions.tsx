ae6d8633602fb588d2f9095d1376b49f24a11676
Paulex <paulalexis.verrier@ynov.com>
2022-04-25 11:41:04 +0200

    changed number of displayed days in weekly and added day number

--- src/components/Conditions/WeeklyConditions.tsx
+++ src/components/Conditions/WeeklyConditions.tsx
@@ -10,22 +10,24 @@ const WeeklyConditions = (props: any) => {
             const { daily } = props.responseObj;
             return daily.map((day: any, index: number) => {
                 var dayname = new Date(day.dt * 1000).toLocaleDateString("fr", {
-                    weekday: "long",
+                    weekday: "long", day: "numeric",
                 });
-                return (
-                    <Card border="secondary" className={classes.Cards} key={index}>
-                        <Card.Img variant="top" src={day.weather.icon} />
-                        <Card.Header>{dayname}</Card.Header>
-                        <Card.Body>
-                            <ListGroup variant="flush">
-                                <ListGroupItem className={classes.Cardstext}>Température : {Math.round(day.temp.day)}°C</ListGroupItem>
-                                <ListGroupItem className={classes.Cardstext}>Description : {day.weather[0].description}</ListGroupItem>
-                                <ListGroupItem className={classes.Cardstext}>Humidité : {day.humidity}%</ListGroupItem>
-                                <ListGroupItem className={classes.Cardstext}>Vent : {day.wind_speed}km/h</ListGroupItem>
-                            </ListGroup>
-                        </Card.Body>
-                    </Card>
-                )
+                if (index > 0) {
+                    return (
+                        <Card border="secondary" className={classes.Cards} key={index}>
+                            <Card.Img variant="top" src={day.weather.icon} />
+                            <Card.Header>{dayname}</Card.Header>
+                            <Card.Body>
+                                <ListGroup variant="flush">
+                                    <ListGroupItem className={classes.Cardstext}>Température : {Math.round(day.temp.day)}°C</ListGroupItem>
+                                    <ListGroupItem className={classes.Cardstext}>Description : {day.weather[0].description}</ListGroupItem>
+                                    <ListGroupItem className={classes.Cardstext}>Humidité : {day.humidity}%</ListGroupItem>
+                                    <ListGroupItem className={classes.Cardstext}>Vent : {day.wind_speed}km/h</ListGroupItem>
+                                </ListGroup>
+                            </Card.Body>
+                        </Card>
+                    )
+                }
             })
         } else {
             return null
